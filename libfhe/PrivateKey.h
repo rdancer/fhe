ec7e87ae8d9cbc8d4e7f914c0959334350d9f9d4
Jan Minář <rdancer@rdancer.org>
2010-08-26 00:29:33 +0100

    Gentry (2008) §3.2 somewhat homomorphic encryption scheme (symmetric) designed as class files

---
+++ libfhe/PrivateKey.h
@@ -0,0 +1,28 @@
+/* PrivateKey.h -- Private key under Epsilon */
+
+/*
+ * Copyright © 2010 Jan Minář <rdancer@rdancer.org>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 (two),
+ * as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "Epsilon.h"
+
+/**
+ * This class implements the private key of Epsilon
+ */
+class PrivateKey {
+    public:
+	PrivateKey(unsigned long long int securityParameter);
+}
